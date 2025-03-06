4e712d4a2d5122a6e606cd0cb28e510e56f00395
규 서문 <illsi@naver.com>
2025-03-06 21:14:02 +0900

    예제 추가

--- MissionLedger/main.cpp
+++ MissionLedger/main.cpp
@@ -1,34 +1,36 @@
-#include <wx/wx.h>
+﻿#include <wx/wx.h>
+#include <wx/listctrl.h>
 
-// wxWidgets 애플리케이션 클래스 정의
-class MyApp : public wxApp {
-public:
-  virtual bool OnInit();
-};
-
-// 메인 윈도우 클래스 정의
 class MyFrame : public wxFrame {
 public:
-  MyFrame() : wxFrame(nullptr, wxID_ANY, "Hello wxWidgets!") {
-    wxPanel* panel = new wxPanel(this, wxID_ANY);
-    wxButton* button = new wxButton(panel, wxID_ANY, "Click Me!", wxPoint(20, 20));
+  MyFrame() : wxFrame(nullptr, wxID_ANY, "wxListCtrl 예제", wxDefaultPosition, wxSize(400, 300)) {
+    wxPanel* panel = new wxPanel(this);
+    wxListCtrl* listCtrl = new wxListCtrl(panel, wxID_ANY, wxPoint(10, 10), wxSize(380, 250),
+                                          wxLC_REPORT);  // 리스트 형식으로 표시
 
-    // 버튼 클릭 이벤트 핸들러
-    button->Bind(wxEVT_BUTTON, &MyFrame::OnButtonClick, this);
-  }
+    // 컬럼(헤더) 추가
+    listCtrl->InsertColumn(0, "이름", wxLIST_FORMAT_LEFT, 100);
+    listCtrl->InsertColumn(1, "나이", wxLIST_FORMAT_LEFT, 50);
+    listCtrl->InsertColumn(2, "직업", wxLIST_FORMAT_LEFT, 100);
 
-private:
-  void OnButtonClick(wxCommandEvent& event) {
-    wxMessageBox("Hello, wxWidgets!", "Info", wxOK | wxICON_INFORMATION);
+    // 데이터 추가
+    long index = listCtrl->InsertItem(0, "홍길동");  // 첫 번째 열 (이름)
+    listCtrl->SetItem(index, 1, "25");  // 두 번째 열 (나이)
+    listCtrl->SetItem(index, 2, "개발자");  // 세 번째 열 (직업)
+
+    index = listCtrl->InsertItem(1, "이순신");
+    listCtrl->SetItem(index, 1, "45");
+    listCtrl->SetItem(index, 2, "군인");
   }
 };
 
-// wxWidgets 애플리케이션 객체 생성
-wxIMPLEMENT_APP(MyApp);
+class MyApp : public wxApp {
+public:
+  virtual bool OnInit() {
+    MyFrame* frame = new MyFrame();
+    frame->Show();
+    return true;
+  }
+};
 
-// 애플리케이션 초기화 함수
-bool MyApp::OnInit() {
-  MyFrame* frame = new MyFrame();
-  frame->Show(true);
-  return true;
-}
+wxIMPLEMENT_APP(MyApp);
