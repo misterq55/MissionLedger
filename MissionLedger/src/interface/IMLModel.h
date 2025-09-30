4c733b101e3444e5b23e2aaafa037b2119855175
규 서문 <illsi@naver.com>
2025-09-30 23:30:05 +0900

    필요없는 메서드 삭제

--- MissionLedger/src/interface/IMLModel.h
+++ MissionLedger/src/interface/IMLModel.h
@@ -7,7 +7,6 @@ class IMLModel
 public:
     virtual void AddTransaction(const SMLTransactionData& transactionData) = 0;
     virtual bool RemoveTransaction(const int transactionId) = 0;
-    virtual int GetTransactionId() = 0;
     virtual bool Save() = 0;
     virtual bool Load() = 0;
 };
